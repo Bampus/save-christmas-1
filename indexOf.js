d8fa28c9728026c76844e2c0229c98815bf0eb4d
Bampus <Hampuswallinder321@gmail.com>
2020-11-23 15:57:36 +0100

    3 commit

--- indexOf.js
+++ indexOf.js
@@ -1,7 +1,12 @@
 
 function indexOf(arr, item) {
-    
-}
+    for (let i = 0; i < array.length; i++) {
+        if(txtval.value==arr[i])
+        return [i]    
+    }
+   
+
+// har inte en susning vad jag ska göra 
 
 const items = [
     'Bicycle',
@@ -17,9 +22,4 @@ console.log(indexOf(items, 'Teddybear'))    // Should be 3
 console.log(indexOf(items, 'Laser sword'))  // Should be -1
 console.log(indexOf(items, 'Playstation'))  // Should be -1
 
-// ------------------
 
-console.log(items.indexOf('Lego'))         // Should be 1
-console.log(items.indexOf('Teddybear'))    // Should be 3
-console.log(items.indexOf('Laser sword'))  // Should be -1
-console.log(items.indexOf('Playstation'))  // Should be -1

4bd4f5ac38ea5db57672710bbf31d19b59ed9711
Bampus <65647017+Bampus@users.noreply.github.com>
2020-11-30 15:17:38 +0100

    commit c

--- map.js
+++ map.js
@@ -1,26 +1,35 @@
 
 function map(arr, func) {
     // skapa en ny tom array av samma längd som arr
-
+    const newArray = []
+    newArray.lenght = arr.lenght 
     // för varje element i arr
     //      newArray[j] = func(arr[j])
 
+    for (let j = 0; j < arr.length; j++) {
+        newArray[j] = func(arr[j])
+    }
     // returnera den nya arrayen
 }
+
 
 // Test 1
 console.log('\nTEST 1')
 const numberArray = [10, 50, 100]
-const newNumberArray = map(numberArray, x => x + 5)
+const newNumberArray = numberArray.map(x => x + 5)
 console.log(newNumberArray) // [15, 55, 105]
 console.log(numberArray)    // [10, 50, 100] 
 
 // Test 2
 console.log('\nTEST 2')
 const stringArray = ['boll', 'häst', 'flygplan']
-const newStringArray = map(stringArray, str => str.toUpperCase())
+const newStringArray = stringArray.map (str => str.toUpperCase())
 console.log(newStringArray) // ['BOLL', 'HÄST', 'FLYGPLAN']
 console.log(stringArray)    // ['boll', 'häst', 'flygplan']
 
 // Test 3 - GÖR ETT EGET TEST HÄR
 console.log('\nTEST 3')
+const nameArray = ["HAMPUS", "WALTER", "BOHAN"]
+const newNameArray = nameArray.map (str => str.toLocaleLowerCase())
+console.log(newNameArray)
+console.log(nameArray)

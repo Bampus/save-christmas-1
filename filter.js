ed585f37a43d8a30810eaacc705a689b178743ac
Bampus <65647017+Bampus@users.noreply.github.com>
2020-11-30 15:30:29 +0100

    commit d

--- filter.js
+++ filter.js
@@ -5,10 +5,13 @@ function filter(arr, func) {
 
 // TEST 1
 const numbers = [4, 10, 3, 12, 5, 16, 8, 13, 2, 9, 1, 11, 2]
-const filteredNumbers = filter(numbers, x => x < 10)
+const filteredNumbers = numbers.filter( x => x < 10)
 console.log(filteredNumbers)
 
 // TEST 2 - skriv själv
 
 
+const numberz = [4, 10, 3, 12, 5, 16, 8, 13, 2, 69, 9, 1, 11, 2]
+const filteredNumberz = numberz.filter( x => x > 9)
+console.log(filteredNumberz)
 // TEST 3 - skriv själv
